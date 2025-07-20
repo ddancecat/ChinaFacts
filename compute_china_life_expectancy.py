5ad3224158bb38b8ea9704ce12f4f9719bce845a
ddancecat <221793505+ddancecat@users.noreply.github.com>
2025-07-20 13:33:03 -0700

    Create compute_china_life_expectancy.py

---
+++ compute_china_life_expectancy.py
@@ -0,0 +1,5 @@
+china_life_expectation_1950 = 43.45 # https://www.macrotrends.net/global-metrics/countries/chn/china/life-expectancy
+china_life_expectation_2025 = 77.81
+life_expectation_changed = china_life_expectation_2025 / china_life_expectation_1950
+print(life_expectation_changed)
+# life_expectation_changed = 1.79079401611
